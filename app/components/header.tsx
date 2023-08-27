0bdbe44fdf6b1b7920b2f2c1017cfc045ca62871
Aitana Hernández <aitana.hernandezpina@gmail.com>
2023-08-27 19:00:59 +0200

    added css to header > nav

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,15 +5,15 @@ import Link from "next/link";
 export const Header = ({ name, pages }) => {
   return (
     <header className="flex flex-col relative bg-linear-557AFF w-full h-48">
-      <div className="flex flex-col justify-around h-full sm:px-6 px:1 bg-header z-10">
-        <nav className="">
+      <div className="bg-header flex flex-col justify-around h-full px-6 z-10">
+        <nav className="text-right flex flex-row gap-6 self-end">
           {pages.map((page) => {
             return (
               <Link
                 aria-label={page.label}
                 key={page.id}
                 href={page.link}
-                className=""
+                className="mt-2 px-6 py-2 min-w-100 bg-lilacmid rounded-lg shadow-lg  hover:bg-lilaclight hover:text-lilacnormal transition ease-in-out duration-500 font-display uppercase tracking-wide"
               >
                 {" "}
                 {page.label}{" "}
@@ -21,7 +21,7 @@ export const Header = ({ name, pages }) => {
             );
           })}
         </nav>
-        <h1 className=""> {name}</h1>
+        <h1 className="text-light uppercase"> {name}</h1>
       </div>
       <Image
         priority
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,78 +1,81 @@
 import type { Config } from 'tailwindcss'
 
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
+module.exports = {
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./app/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
     colors: {
-      light: "f4effa",
-      black: "000000",
-      lilacdarker: "2f184b",
-      lilacnormal: "532b88",
-      lilacmid: "9b72cf",
-      lilaclight: "c8b1e4"
+      light: "#f4effa",
+      black: "#000000",
+      lilacdarker: "#2f184b",
+      lilacnormal: "#532b88",
+      lilacmid: "#9b72cf",
+      lilaclight: "#c8b1e4"
     },
-    fontFamily: {
-      sans: ['Georgia', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-      display: ['Oswald'],
-    },
-    fontSize: {
-      sm: "0.8rem",
-      base: "1rem",
-      lg: "1.15rem",
-      xl: "1.25rem",
-      "2xl": "1.5rem",
-      "3xl": "1.875rem",
-      "4xl": "2.25rem",
-      "5xl": "3rem",
-      "6xl": "4rem",
-    },
-    minWidth: { 
-      0: "0",
-      100: "100px",
-    },
-    extend: {
-      width: {
-        "1/3-g": "30%",
-       },
-       minHeight: {
-        50: "55px",
-       },
-       boxShadow: {
-        footer: "0px -7px 41px rgba(175, 175, 175, 0.25)",
-       },
-       backgroundImage: {
-        header: "linear-gradient(98.85deg, #557AFF 7.8%, rbga(239, 252, 255, 0) 100%)",
-       },
-       spacing: {
-        "8xl": "96rem",
-        "9xl": "128rem",
-       },
-       borderRadius: {
-        "4xl": "2rem",
-       },
-       flexBasis: {
-        "4/5": "66%",
-       },
-      
-       screens: {
-        sm: "640px",
-        // => @media (min-width: 640px)
-        md: "768px",
-        // => @media (min-width: 768px)
-        ld: "1024px",
-        // => @media (min-width: 1024px)
-        xl: "1280px",
-        // => @media (min-width: 1280px)
-        "2xl": "1536px",
-        // => @media (min-width: 1536px)
-       },
-    },
-  },
-  plugins: [],
-}
-export default config
+		fontFamily: {
+			sans: ["Georgia", "sans-serif"],
+			serif: ["Merriweather", "serif"],
+			display: ["Montserrat", "sans-serif"],
+		},
+		fontSize: {
+			sm: "0.8rem",
+			base: "1rem",
+			lg: "1.15rem",
+			xl: "1.25rem",
+			"2xl": "1.563rem",
+			"3xl": "1.953rem",
+			"4xl": "2.441rem",
+			"5xl": "3.052rem",
+			"6xl": "3.5rem",
+		},
+		minWidth: {
+			0: "0",
+			100: "100px",
+		},
+		extend: {
+			width: {
+				"1/3-g": "30%",
+			},
+			minHeight: {
+				50: "55px",
+			},
+			boxShadow: {
+				footer: "0px -7px 41px 3px rgba(175, 175, 175, 0.25)",
+			},
+			backgroundImage: {
+				header:
+					"linear-gradient(98.85deg, #532b88 7.8%, rgba(239, 252, 255, 0) 139.35%);",
+			},
+			spacing: {
+				"8xl": "96rem",
+				"9xl": "128rem",
+			},
+			borderRadius: {
+				"4xl": "2rem",
+			},
+			flexBasis: {
+				"4/5": "66%",
+			},
+		},
+		screens: {
+			sm: "640px",
+			// => @media (min-width: 640px) { ... }
+
+			md: "768px",
+			// => @media (min-width: 768px) { ... }
+
+			lg: "1024px",
+			// => @media (min-width: 1024px) { ... }
+
+			xl: "1280px",
+			// => @media (min-width: 1280px) { ... }
+
+			"2xl": "1536px",
+			// => @media (min-width: 1536px) { ... }
+		},
+	},
+	plugins: [],
+};
