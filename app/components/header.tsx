2cacf033c7986c95e377802eb5454c8a1d36bc92
Aitana Hernández <aitana.hernandezpina@gmail.com>
2023-09-20 16:41:03 +0200

    WIP responsive menu

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,9 +3,11 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { Github, Linkedin } from "../icons";
+import { Github, Linkedin, menuIcon, closeIcon } from "../icons";
+
 import { contactMeLinks } from "../data/data";
 import useScrollToAnchor from "../hooks/useScrollToAnchor";
+import { useState } from "react";
 
 type HeaderProps = {
   name: string;
@@ -18,10 +20,20 @@ type HeaderProps = {
 };
 
 export const Header = ({ name, pages, contactMeLinks }: HeaderProps) => {
+  // Scroll to anchor
   const scrollToAnchor = useScrollToAnchor(80);
+  // Mobile menu
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   const setActive = (link: string) => {
     scrollToAnchor(link);
+
+    // Cierra el menú móvil cuando se hace clic en un elemento del menú
+    setMobileMenuOpen(false);
   };
 
   return (
@@ -29,28 +41,42 @@ export const Header = ({ name, pages, contactMeLinks }: HeaderProps) => {
       <div className="bg-header backdrop-blur-sm flex flex-col sm:fixed sm:top-0 sm:left-0 sm:w-full sm:h-20 px-6 z-10">
         <div className="flex items-center justify-between gap-3 h-full">
           <nav className="text-center sm:text-right flex flex-col sm:flex-row sm:gap-6 sm:mb-5 gap-2 mb-2 self-end">
-            {pages.map((page) => {
-              return (
-                // <Link
-                //   aria-label={page.label}
-                //   key={page.id}
-                //   onClick={() => setActive(page.link)}
-                //   href={`#${page.id}`}
-                //   className="mt-2 px-6 py-2 min-w-100 bg-lilacmid rounded-lg shadow-lg hover:bg-lilaclight hover:text-lilacnormal transition ease-in-out duration-500 font-display uppercase tracking-wide"
-                // >
-                //   {page.label}
-                // </Link>
+            {/* Renderiza los botones en la versión de navegador */}
+            <div className="hidden sm:block">
+              {pages.map((page) => (
                 <button
-                  aria-label={page.label}
                   key={page.id}
                   onClick={() => setActive(page.link)}
                   className="mt-2 px-6 py-2 min-w-100 bg-lilacmid rounded-lg shadow-lg hover:bg-lilaclight hover:text-lilacnormal transition ease-in-out duration-500 font-display uppercase tracking-wide"
                 >
                   {page.label}
                 </button>
-              );
-            })}
+              ))}
+            </div>
           </nav>
+          {/* Renderiza el menú hamburguesa en dispositivos móviles */}
+          <div className="block sm:hidden">
+            {isMobileMenuOpen ? (
+              pages.map((page) => (
+                <button
+                  key={page.id}
+                  onClick={() => setActive(page.link)}
+                  className="block mt-2 px-6 py-2 min-w-100 bg-lilacmid rounded-lg shadow-lg hover:bg-lilaclight hover:text-lilacnormal transition ease-in-out duration-500 font-display uppercase tracking-wide"
+                >
+                  {page.label}
+                </button>
+              ))
+            ) : (
+              <button onClick={toggleMobileMenu}>
+                <Linkedin className="icons_contactme" />{" "}
+              </button>
+            )}
+          </div>
+          {isMobileMenuOpen && (
+            <button onClick={toggleMobileMenu} className="sm:hidden">
+              <Github className="icons_contactme" />{" "}
+            </button>
+          )}
           <div className="flex gap-4 mr-5 px-6 py-2 min-w-100 bg-lilacdarker rounded-lg shadow-lg">
             <a href={contactMeLinks[0]} aria-label="GitHub">
               <Github className="icons_contactme" />
---
+++ app/icons/menuIcon.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const MenuIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path
+        d="M2 6h20M2 12h20M2 18h20"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default MenuIcon;
